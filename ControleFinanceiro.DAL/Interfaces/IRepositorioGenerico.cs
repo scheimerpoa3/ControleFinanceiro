23ccb983406d60215241e94f4656e822279dca33
Luiz Gustavo Scheimer <scheimerpoa3@hotmail.com>
2021-11-18 09:34:19 -0300

    repositorio Genérico

---
+++ ControleFinanceiro.DAL/Interfaces/IRepositorioGenerico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.DAL.Interfaces
+{
+	public interface IRepositorioGenerico<TEntity> where TEntity : class
+	{
+		IQueryable<TEntity> PegarTodos();
+		Task<TEntity> PegarPeloId(int id);
+		Task<TEntity> PegarPeloId(string id);
+		Task Inserir(TEntity entity);
+		Task Inserir(List<TEntity> entity);
+		Task Atualizar(TEntity entity);
+		Task Excluir(String id);
+		Task Excluir(int id);
+	}
+}
